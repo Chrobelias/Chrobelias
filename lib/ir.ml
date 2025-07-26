49ab4abcbc8052b0271824bd7ce4bb2809f7a049
Kakadu <Kakadu@pm.me>
2025-07-26 19:53:18 +0300

    [feat] Monotonicity-based simplifier
    Fix #68
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- lib/ir.ml
+++ lib/ir.ml
@@ -75,8 +75,7 @@ let rec equal ir ir' =
 ;;
 
 module X = struct
-  type t' = t
-  type t = t'
+  type nonrec t = t
 
   let rec equal ir ir' =
     match ir, ir' with
@@ -172,6 +171,20 @@ let rec fold f acc ir =
   | Exists (_, ir') -> f (fold f acc ir') ir
 ;;
 
+let is_used_atom (v : string) inside =
+  let exception Found in
+  try
+    fold
+      (fun () -> function
+         | Rel (_, mapa, _) when Map.mem mapa (Var v) -> raise Found
+         | _ -> ())
+      ()
+      inside;
+    false
+  with
+  | Found -> true
+;;
+
 let for_all f ir = fold (fun acc ir -> f ir |> ( && ) acc) true ir
 let for_some f ir = fold (fun acc ir -> f ir |> ( || ) acc) false ir
 
@@ -234,9 +247,8 @@ let pp_smtlib ppf (ir : t) =
   in
   let smtml_ir =
     match ir with
-    | Land xs ->
-      Printf.eprintf "%s %d\n" __FILE__ __LINE__;
-      exit 1
+    | Land (x :: xs) ->
+      Smtml.Ast.Assert (Expr.Bool.and_ (expr_of_ir x) (expr_of_ir (Land xs)))
     | Exists (atoms, xs) ->
       Smtml.Ast.Assert (Smtml.Expr.exists (List.map of_atom atoms) (expr_of_ir xs))
     | _ ->
@@ -245,4 +257,213 @@ let pp_smtlib ppf (ir : t) =
   in
   let open Smtml.Ast in
   pp ppf smtml_ir
+;;
+
+(** A manually implemented printer to SMTLIB2-like format *)
+let pp_smtlib2 ppf ir =
+  let open Format in
+  let rec helper ppf = function
+    | True -> fprintf ppf "T"
+    | Exists (atoms, rhs) ->
+      fprintf
+        ppf
+        "@[(exists (%a)@ %a@]@ "
+        (Format.pp_print_list ~pp_sep:Format.pp_print_space pp_atom)
+        atoms
+        helper
+        rhs
+    | Land [ x ] ->
+      (* TODO: should be eliminated in simplifier *)
+      helper ppf x
+    | Land xs ->
+      fprintf ppf "@[<v 2>@[(and@]@ ";
+      List.iter (helper ppf) xs;
+      fprintf ppf "@]"
+    | Rel (op, poly, rhs) ->
+      let pp_map ppf mapa =
+        let one =
+          fun ~key ~data ->
+          match data with
+          | 1 -> fprintf ppf "%a@ " pp_atom key
+          | _ when data > 0 -> fprintf ppf "(* %d %a)@ " data pp_atom key
+          | _ -> fprintf ppf "(* (- %d) %a)@ " (-data) pp_atom key
+        in
+        if Map.length mapa = 1
+        then (
+          let v, coeff = Map.min_elt_exn mapa in
+          one ~key:v ~data:coeff)
+        else (
+          fprintf ppf "@[(+ ";
+          Map.iteri mapa ~f:one;
+          fprintf ppf ")@]@ ")
+      in
+      fprintf
+        ppf
+        "@[(%s %a %d)@]@ "
+        (match op with
+         | Leq -> "<="
+         | Eq -> "=")
+        pp_map
+        poly
+        rhs
+    | _ ->
+      Printf.eprintf "%s %d\n" __FILE__ __LINE__;
+      exit 1
+  in
+  match ir with
+  | Land xs ->
+    fprintf ppf "@[<v>";
+    List.iter (fprintf ppf "@[(assert %a)@]@," helper) xs;
+    fprintf ppf "@]"
+  | ir -> fprintf ppf "(assert %a)" helper ir
+;;
+
+type from =
+  | Top
+  | Bot
+
+(** [Bound (Top, x, y)] means for variable [v] holds [v <= x/y]
+    [Bound (Bot, x, y)] means for variable [v] holds [v >= x/y] *)
+type bound = from * int * int
+
+let pp_bound ppf = function
+  | Top, x, y -> Format.fprintf ppf "<= %d/%d" x y
+  | Bot, x, y -> Format.fprintf ppf ">= %d/%d" x y
+;;
+
+type verdict =
+  | Skip
+  | Stop
+  | Pos
+  | Neg (* varible is in linear combination with [negative] coeff *)
+  | Bound of bound
+
+let log ppf =
+  match Sys.getenv "CHRO_DEBUG" with
+  | exception Not_found -> Format.ifprintf Format.std_formatter ppf
+  | _ -> Format.kasprintf (Format.printf "%s\n%!") ppf
+;;
+
+(** Habermehl's 2024 monotonicity simplification  *)
+let simpl_monotonicty ir =
+  log "ir = %a" pp ir;
+  let is_bounded qvar ir =
+    match ir with
+    | Rel (Leq, map, rhs) when Map.length map = 1 ->
+      let var, coeff = Map.min_elt_exn map in
+      if var = Var qvar
+      then Bound (if coeff > 0 then Top, rhs, coeff else Bot, rhs, coeff)
+      else Skip
+    | Rel (Leq, map, _) ->
+      (match Map.find map (Var qvar) with
+       | None -> Skip
+       | Some c when c > 0 -> Pos
+       | _ -> Neg)
+    | _ when is_used_atom qvar ir -> Stop
+    | _ -> Skip
+  in
+  match ir with
+  | Exists (atoms, rhs) ->
+    let vars, other_atoms =
+      List.fold_left
+        (fun (vars, other) atom ->
+           match atom with
+           | Var v -> v :: vars, other
+           | o -> vars, o :: other)
+        ([], [])
+        atoms
+    in
+    if vars <> [] then log "Vars: %s" ([%show: string list] vars);
+    if other_atoms <> [] then log "Other: %s" ([%show: atom list] other_atoms);
+    let rewrite_rel v new_value ir =
+      let ans =
+        match ir with
+        | Rel (r, mapa, rhs) ->
+          let coeff = Map.find_exn mapa (Var v) in
+          Rel (r, Map.remove mapa (Var v), rhs - (coeff * new_value))
+        | ir -> ir
+      in
+      log "%a ~~> %a using %s=%d" pp ir pp ans v new_value;
+      ans
+    in
+    let rec loop ~progress ivars ovars conjs ~sk =
+      let _ : string list = ovars in
+      match ivars with
+      | [] -> sk progress ovars conjs
+      | v :: ivars ->
+        let exception Stop in
+        (* log "Check %s" v; *)
+          (try
+             let verdict =
+               List.fold_left
+                 (fun (bounds, pos, neg, skipped) c ->
+                    match is_bounded v c with
+                    | Bound x -> x :: bounds, pos, neg, skipped
+                    | Pos -> bounds, c :: pos, neg, skipped
+                    | Neg -> bounds, pos, c :: neg, skipped
+                    | Skip -> bounds, pos, neg, c :: skipped
+                    | Stop -> raise Stop)
+                 ([], [], [], [])
+                 conjs
+             in
+             let verdict =
+               let bounds, pos, negs, rest = verdict in
+               let min, max =
+                 List.fold_left
+                   (fun acc b ->
+                      match acc, b with
+                      | (low, None), (Top, b, a) -> low, Some (b / a)
+                      | (None, high), (Bot, b, a) -> Some (b / a), high
+                      | (low, Some m), (Top, b, a) -> low, Some (min m (b / a))
+                      | (Some m, high), (Bot, b, a) -> Some (max m (b / a)), high)
+                   (None, None)
+                   bounds
+               in
+               min, max, pos, negs, rest
+             in
+             match verdict with
+             | None, None, _, _, _ ->
+               log "Var %s is not monotonic\n%!" v;
+               loop ~progress ivars (v :: ovars) conjs ~sk
+             | None, Some _, _ :: _, _, other | Some _, None, _, _ :: _, other ->
+               (* Can't simplify  *)
+               log "Can't simplify %s: bad polarity" v;
+               loop ~progress ivars (v :: ovars) conjs ~sk
+             | None, Some high, [], negs, other ->
+               log "Simplifying %s..." v;
+               let negs = List.map (rewrite_rel v high) negs in
+               loop ~progress:true ivars ovars (negs @ other) ~sk
+             | Some low, None, pos, [], other ->
+               log "Simplifying %s..." v;
+               let pos = List.map (rewrite_rel v low) pos in
+               loop ~progress:true ivars ovars (pos @ other) ~sk
+             | Some low, Some high, pos, negs, other ->
+               log "Simplifying %s..." v;
+               let pos = List.map (rewrite_rel v low) pos in
+               let negs = List.map (rewrite_rel v high) negs in
+               loop ~progress:true ivars ovars (pos @ negs @ other) ~sk
+           with
+           | Stop ->
+             log "Var %s can't be interesting: used somewhere" v;
+             loop ~progress ivars (v :: ovars) conjs ~sk)
+    in
+    let rhs =
+      match rhs with
+      | Land xs -> xs
+      | x -> [ x ]
+    in
+    let rec fixpoint stage vars other_vars rhs =
+      loop ~progress:false vars [] rhs ~sk:(fun progress ovars rhs ->
+        match progress, ovars with
+        | false, _ | _, [] -> Exists (List.map var ovars @ other_atoms, Land rhs)
+        | true, ovars ->
+          log
+            "After stage %d there %d variables: %s"
+            stage
+            (List.length ovars)
+            (String.concat " " ovars);
+          fixpoint (stage + 1) ovars [] rhs)
+    in
+    fixpoint 0 vars [] rhs
+  | _ -> ir
 ;;
