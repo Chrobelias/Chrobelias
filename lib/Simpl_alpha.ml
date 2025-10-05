afda103c937249f7a517168556889e0d536083b1
Kakadu <Kakadu@pm.me>
2025-10-05 18:05:57 +0300

    Disable alpha-eqiuvalence optimization
    It currently crashes.
    And it should be place near AST, not near IR
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- lib/Simpl_alpha.ml
+++ lib/Simpl_alpha.ml
@@ -1,5 +1,19 @@
 type subst = (string, string, Base.String.comparator_witness) Base.Map.t
 
+let rec walk s name =
+  match Base.Map.find s name with
+  | Some x -> walk s x
+  | None -> name
+;;
+
+let unify subst l1 l2 =
+  let l1 = walk subst l1 in
+  let l2 = walk subst l2 in
+  if String.equal l1 l2
+  then Some subst
+  else Some (Base.Map.add_exn subst ~key:l1 ~data:l2)
+;;
+
 let pp_subst : Format.formatter -> subst -> unit =
   fun ppf s ->
   Format.fprintf ppf "@[{| ";
@@ -19,13 +33,14 @@ let pp_poly : Format.formatter -> (Ir.atom, int) Ir.Map.t -> unit =
 open Ir
 
 let alpha_compare =
+  let exception Exit of int in
   let rec helper (subst : subst) (l, r) =
     match l, r with
     | Lnot l, Lnot r -> helper subst (l, r)
-    | Rel (Eq, _, _), (Rel (Leq, _, _) | SEq _ | SReg _)
-    | SLen _, (SEq _ | SReg _)
+    | Rel (Eq, _, _), (Rel (Leq, _, _) | SEq _ | SReg _ | Itos _)
+    | SLen _, (SEq _ | SReg _ | True | Reg _ | Itos _ | Lnot _ | Land _ | Lor _)
     | Rel (Eq, _, _), SLen (_, _)
-    | Rel (Leq, _, _), SLen (_, _)
+    | Rel (Leq, _, _), (SLen (_, _) | Itos _)
     | SEq (_, _), Exists (_, _)
     | ( SEq (_, _)
       , ( True
@@ -33,6 +48,7 @@ let alpha_compare =
         | SReg (_, _)
         | SLen (_, _)
         | SEq (_, _)
+        | Itos (_, _)
         | Stoi (_, _)
         | Lnot _ | Land _ | Lor _ ) )
     | (Rel (Leq, _, _) | SEq _), Rel (Eq, _, _)
@@ -47,10 +63,33 @@ let alpha_compare =
     | Rel _, Land _
     | Lor _, Rel _
     | Rel _, Lor _
-    | Stoi _, SReg _
-    | Lnot _, (SReg (_, _) | SLen (_, _) | SEq (_, _) | Stoi (_, _))
+    | ( Stoi _
+      , ( True
+        | Reg (_, _)
+        | SLen (_, _)
+        | SEq (_, _)
+        | SReg _ | Exists _
+        | Itos (_, _)
+        | Lnot _ | Land _ | Lor _ ) )
+    | Lnot _, (SReg (_, _) | SLen (_, _) | SEq (_, _) | Stoi (_, _) | Itos _)
     | Lnot _, (True | Reg (_, _) | Land _ | Lor _ | Exists _)
-    | Exists _, SLen _
+    | ( Reg (_, _)
+      , ( True
+        | Exists (_, _)
+        | SReg (_, _)
+        | SLen (_, _)
+        | SEq (_, _)
+        | Stoi (_, _)
+        | Itos (_, _)
+        | Lnot _ | Land _ | Lor _ ) )
+    | ( Exists _
+      , ( SLen _ | True
+        | Reg (_, _)
+        | SReg (_, _)
+        | SEq (_, _)
+        | Stoi (_, _)
+        | Itos (_, _)
+        | Land _ | Lor _ ) )
     | SLen _, (Exists _ | Rel _ | Stoi _)
     | Rel (_, _, _), True
     | Exists _, Lnot _
@@ -63,6 +102,32 @@ let alpha_compare =
       let rez = Z.compare c1 c2 in
       if rez = 0 then helper_polyn subst l r else rez
     | Land xs, Land ys | Lor xs, Lor ys -> helper_lists subst (xs, ys)
+    | Stoi (Var l1, Var r1), Stoi (Var l2, Var r2)
+    | SLen (Var l1, Var r1), SLen (Var l2, Var r2) ->
+      let l1 = walk subst l1 in
+      let l2 = walk subst l2 in
+      (match unify subst l1 l2 with
+       | None -> String.compare l1 l2
+       | Some subst ->
+         let r1 = Base.Map.find subst r1 |> Option.value ~default:r1 in
+         let r2 = Base.Map.find subst r2 |> Option.value ~default:r2 in
+         (match unify subst r1 r2 with
+          | None -> String.compare r1 r2
+          | Some _ -> String.compare r1 r2))
+    | Exists (Var _ :: [], _), Exists (Pow2 _ :: [], _)
+    | Stoi (Var _, Var _), Stoi (Pow2 _, _)
+    | Stoi (Var _, Var _), Stoi (Var _, Pow2 _)
+    | SLen (Var _, Var _), SLen (Pow2 _, _)
+    | SLen (Var _, Var _), SLen (Var _, Pow2 _) -> raise (Exit (-1))
+    | Stoi (Pow2 _, _), Stoi (_, _)
+    | Stoi (Var _, Pow2 _), Stoi (_, _)
+    | SLen (Pow2 _, _), SLen (_, _)
+    | SLen (Var _, Pow2 _), SLen (_, _) -> 1
+    | Exists (Pow2 _ :: [], _), Exists (_ :: [], _) -> 1
+    | Exists ([], _), Exists (_, _) | Exists (_, _), Exists ([], _) ->
+      (* TODO(Kakadu): change representation c *)
+      failwith "Should not happen"
+    (* and helper_atom subst l r =  *)
     | l, r ->
       Format.eprintf "Not implemented comparison:\n\t%a\n\t%a\n%!" Ir.pp l Ir.pp r;
       assert false
@@ -85,19 +150,18 @@ let alpha_compare =
     in
     let l = normalize subst l in
     let r = normalize subst r in
-    (* Debug.printfln "  norm_l = %a" pp_poly l;
-    Debug.printfln "  norm_r = %a" pp_poly r; *)
     let ans2 = Ir.Map.compare_direct Stdlib.compare l r in
-    (* Debug.printfln "   ans2 = %d" ans2; *)
     ans2
   in
   fun a b ->
-    (* Debug.printfln "WTF: l = %a" Ir.pp a;
-    Debug.printfln "     r = %a" Ir.pp b; *)
-    let ans = helper (Base.Map.empty (module Base.String)) (a, b) in
-    (* Debug.printfln "   ans = %d" ans; *)
-    ans
+    try
+      let _ = helper (Base.Map.empty (module Base.String)) (a, b) in
+      0
+    with
+    | Exit n -> n
 ;;
+
+(* Debug.printfln "   ans = %d" ans; *)
 
 let rec simplify : Ir.t -> Ir.t = function
   | True -> True
