14fa8866a3ad71c5e7c8935a6a569cd25324d78e
Mikhail Starchak <m.starchak@spbu.ru>
2025-12-03 21:54:02 +0300

    Handle non-linearity

--- lib/Underapprox.ml
+++ lib/Underapprox.ml
@@ -203,7 +203,9 @@ let check bound ast =
            let ph = apply_symantics sym ast in
            let module Z3 = Smtml.Z3_mappings.Solver in
            (* let module Z3 = Smtml.Cvc5_mappings.Solver in *)
-           let solver = Z3.make ~params:Smtml.Params.(default () $ (Timeout, 20000)) () in
+           let solver =
+             Z3.make ~params:Smtml.Params.(default () $ (Timeout, 200000)) ()
+           in
            Z3.reset solver;
            let __ _ = log "Into Z3 goes: @[%a@]\n%!" Smtml.Expr.pp ph in
            match Z3.check solver ~assumptions:[ ph ] with
