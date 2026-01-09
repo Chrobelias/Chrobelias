aeeb60447e56d94d99d4e2ed197c0cb9a3a6ef71
Georgiy Belyanin <belyaningeorge@ya.ru>
2026-01-09 20:53:32 +0300

    solver: speedup chrob.len predicate
    This patch speeds up evaluation of the `(chrob.len s 2^x) predicate that
    is used within Chrobelias in order to represent that `x` is a length of
    `s`. The key idea is not to introduce a separate variable for `2^x` and
    leave it as a power of two.

--- lib/solver.ml
+++ lib/solver.ml
@@ -1019,14 +1019,16 @@ struct
             Ir.rel rel term c
           | SReg (atom, re) when Map.mem map atom -> Ir.true_
           | SRegRaw (atom, re) when Map.mem map atom -> Ir.true_
-          | SLen (atom, atom') when Map.mem map atom ->
+          | SLen (atom, atom') when Map.mem map (get_exp atom') ->
             let new_atom = Ir.internal () in
-            let v = Extra.model_to_int (Map.find_exn map atom) in
-            Ir.land_ [ Ir.slen new_atom atom'; Ir.eq (Map.singleton new_atom Z.one) v ]
-          | SLen (atom, atom') when Map.mem map atom' ->
+            let v = Extra.model_to_int (Map.find_exn map (get_exp atom')) in
+            Ir.land_
+              [ Ir.slen atom new_atom; Ir.eq (Map.singleton new_atom Z.one) (pow2z v) ]
+          (*| SLen (atom, atom') when mem_var_or_pow atom ->
+          | SLen (atom, atom') when mem_var_or_pow atom' ->
             let new_atom' = Ir.internal () in
-            let v = Extra.model_to_int (Map.find_exn map atom') in
-            Ir.land_ [ Ir.slen atom new_atom'; Ir.eq (Map.singleton new_atom' Z.one) v ]
+            let v = Extra.model_to_int (Map.find_exn map (get_exp atom')) in
+            Ir.land_ [ Ir.slen atom new_atom'; Ir.eq (Map.singleton new_atom' Z.one) v ]*)
           (*| Ast.Var x -> *)
           (*  Base.Option.value ~default:(Ast.Var x) (Map.find map x |> Option.map Ast.const) *)
           (*| Ast.Pow (2, Ast.Const c) -> Ast.Const (pow2 c) *)
