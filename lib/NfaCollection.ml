742b4ab3e3e0abaafacb677225d63e85255fff4c
Georgiy Belyanin <belyaningeorge@ya.ru>
2026-01-12 23:41:54 +0300

    bugfix

--- lib/NfaCollection.ml
+++ lib/NfaCollection.ml
@@ -872,7 +872,6 @@ module MsbStr = struct
          ~vars:(List.map fst term)
          ~deg:(1 + List.fold_left Int.max 0 (List.map fst term))
        |> fun x -> x)
-      |> Nfa.minimize_strong
   ;;
 end
 
