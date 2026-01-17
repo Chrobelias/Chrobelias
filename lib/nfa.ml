e431b2c8579b8a145579fd51ade75a1b2f8436ba
Mikhail Starchak <mikhstark@gmail.com>
2026-01-17 17:02:23 +0300

    [nfa] shrink, new attempt

--- lib/nfa.ml
+++ lib/nfa.ml
@@ -1603,46 +1603,62 @@ struct
   ;;
 
   let shrink (nfa : t) =
-    let transitions =
+    let shrink delta =
+      if List.is_empty delta
+      then delta
+      else (
+        let state = delta |> List.hd |> snd in
+        delta
+        |> List.map (fun (label, _) -> label)
+        |> List.map (fun label1 ->
+          ( 0 -- nfa.deg
+            |> List.fold_left
+                 (fun acc i ->
+                    let label1 = acc in
+                    let symbol1 = Label.get label1 i in
+                    let label1' = Label.project [ i ] label1 in
+                    let flag = ref false in
+                    let symbols =
+                      List.fold_left
+                        (fun acc (label2, q2) ->
+                           if state = q2 && Label.equal label2 label1'
+                           then
+                             if Label.is_any_at i label2
+                             then (
+                               flag := true;
+                               acc)
+                             else (
+                               let symbol2 = Label.get label2 i in
+                               Set.add acc symbol2)
+                           else acc)
+                        (Set.singleton symbol1)
+                        delta
+                    in
+                    if
+                      !flag
+                      || Label.alphabet
+                         |> List.take (Z.to_int (Config.base ()))
+                         |> List.for_all (fun c -> Set.mem symbols c)
+                    then label1'
+                    else label1)
+                 label1
+          , state )))
+    in
+    let transitions' =
       nfa.transitions
       |> Array.map (fun delta ->
-        List.map
-          (fun (label1, q1) ->
-             ( 0 -- nfa.deg
-               |> List.fold_left
-                    (fun acc i ->
-                       let label1 = acc in
-                       let symbol1 = Label.get label1 i in
-                       let label1' = Label.project [ i ] label1 in
-                       let flag = ref false in
-                       let symbols =
-                         List.fold_left
-                           (fun acc (label2, q2) ->
-                              if q1 = q2 && Label.equal label2 label1'
-                              then
-                                if Label.is_any_at i label2
-                                then (
-                                  flag := true;
-                                  acc)
-                                else (
-                                  let symbol2 = Label.get label2 i in
-                                  Set.add acc symbol2)
-                              else acc)
-                           (Set.singleton symbol1)
-                           delta
-                       in
-                       if
-                         !flag
-                         || Label.alphabet
-                            |> List.take (Z.to_int (Config.base ()))
-                            |> List.for_all (fun c -> Set.mem symbols c)
-                       then label1'
-                       else label1)
-                    label1
-             , q1 ))
-          delta)
+        let length = length nfa in
+        let states =
+          Graph.reachable_in_range nfa.transitions 0 ((length * length) - 1) nfa.start
+          |> List.fold_left (fun acc x -> Set.union acc x) Set.empty
+        in
+        states
+        |> Set.to_list
+        |> List.map (fun x -> List.filter (fun (label, state) -> state = x) delta)
+        |> List.map shrink
+        |> List.concat)
     in
-    { nfa with transitions }
+    { nfa with transitions = transitions' }
   ;;
 end
 
@@ -1919,6 +1935,7 @@ module Lsb (Label : L) = struct
     }
     |> remove_unreachable_from_final
     |> remove_unreachable_from_start
+    |> shrink
   ;;
 
   let minimize_strong nfa =
@@ -1929,6 +1946,7 @@ module Lsb (Label : L) = struct
     |> to_dfa
     |> reverse
     |> to_dfa
+    |> shrink
   ;;
 
   let minimize_not_very_strong nfa =
