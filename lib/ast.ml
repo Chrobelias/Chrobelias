8d0f47fd17b873cd5a3101a38ba8a0358efe8f41
Mikhail Starchak <mikhstark@gmail.com>
2026-01-18 16:07:55 +0300

    [ast] semantics of exp in to_nat

--- lib/ast.ml
+++ lib/ast.ml
@@ -800,8 +800,11 @@ let to_nat ast =
               (Eia.map2
                  Fun.id
                  (function
+                   | Eia.Pow (Eia.Const base, Eia.Atom (Var (v, I))) when v = var ->
+                     Eia.const Z.zero
                    | Eia.Atom (Var (v, I)) as v' when v = var ->
-                     Eia.Mul [ v'; Eia.const Z.minus_one ]
+                     Eia.Add
+                       [ Eia.Mul [ v'; Eia.const Z.minus_one ]; Eia.const Z.minus_one ]
                    | eia' -> eia')
                  Fun.id
                  eia')
