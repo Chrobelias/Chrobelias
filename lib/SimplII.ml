3ac88aecef388af0d112e208ce20cd017418715f
Mikhail Starchak <mikhstark@gmail.com>
2026-03-05 12:54:43 +0300

    [simpl] seed for z3

--- lib/SimplII.ml
+++ lib/SimplII.ml
@@ -2214,7 +2214,7 @@ let check_nia env ast =
     Z3.make
       ~logic:Smtml.Logic.QF_NIA
       ()
-      ~params:Smtml.Params.(default () $ (Timeout, 200000))
+      ~params:Smtml.Params.(default () $ (Timeout, 200000) $ (Random_seed, 42))
   in
   Z3.reset solver;
   match Z3.check solver ~assumptions:[ ph ] with
