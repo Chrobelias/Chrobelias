b0a2103e0b89dff4a72d498f1aca5c12cabb51f8
Mikhail Starchak <mikhstark@gmail.com>
2026-03-05 12:54:43 +0300

    [simpl] seed for z3

--- benchmarks/gen.ml
+++ benchmarks/gen.ml
@@ -331,8 +331,6 @@ let prepare_script ?(opp = Swine) ~script () =
     printfn "export TIMEOUT=%d" config.timeout;
     (* printfn "SECONDS=0"; *)
     printfn "printf '\n%s (%d/%d)...\n'" smt2file curi total;
-    printfn "APPROX='-bound 10 -over-early'";
-    printfn "FLAT='-flat 0 -amin 0 -amax 100000'";
     printfn
       "if timeout $TIMEOUT time -f 'THETIME %%U' dune exec Chro --profile=release -- \
        $APPROX $FLAT %s -q %s > .log 2> .errlog"
