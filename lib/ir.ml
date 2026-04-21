9b88859e7a456c825cd4ba3bfd925da51885fb34
Mikhail Starchak <mikhstark@gmail.com>
2026-04-21 11:41:25 +0300

    [ir] antiprenex modes

--- lib/ir.ml
+++ lib/ir.ml
@@ -596,14 +596,17 @@ let collect_free (ir : t) =
 
 let antiprenex =
   fun ir ->
-  map
-    (function
-      | Exists ([], ir) -> ir
-      | Exists (atoms, Exists (atoms', ir)) ->
-        exists (Base.List.dedup_and_sort ~compare (atoms @ atoms')) ir
-      | Exists ((a :: b :: tl as atoms), Land irs) as orig_ir ->
-        let atoms =
-          (*List.filter
+  if Config.config.antiprenex_mode = `Disable
+  then ir
+  else
+    map
+      (function
+        | Exists ([], ir) -> ir
+        | Exists (atoms, Exists (atoms', ir)) ->
+          exists (Base.List.dedup_and_sort ~compare (atoms @ atoms')) ir
+        | Exists ((a :: b :: tl as atoms), Land irs) as orig_ir ->
+          let atoms =
+            (*List.filter
               (fun atom ->
                  not
                    (for_some
@@ -615,64 +618,64 @@ let antiprenex =
                           when atom = atom' -> true
                         | _ -> false)
                       ir))*)
-          atoms
-        in
-        let atoms_set = Set.of_list atoms in
-        if atoms_set |> Set.is_empty
-        then orig_ir
-        else (
-          let irs_using_var : (int * atom Set.t) list =
-            List.mapi
-              begin fun i ir ->
-                let free_vars = collect_free ir in
-                let used_vars = Set.inter atoms_set free_vars in
-                i, used_vars
-              end
-              irs
+            atoms
           in
-          let var_is_used_in : (atom, int list) Map.t =
-            List.map
-              begin fun atom ->
-                ( atom
-                , List.filter_map
-                    (fun (i, s) ->
-                       if
-                         Set.mem s atom
-                         || (Config.config.antiprenex_mode = `Lower_re
-                             && List.nth irs i
-                                |> function
-                                | SRegRaw _ -> true
-                                | _ -> false)
-                       then Some i
-                       else None)
-                    irs_using_var )
-              end
-              atoms
-            |> Map.of_alist_exn
-          in
-          let atom_to_move, used_in =
-            var_is_used_in
-            |> Map.to_alist
-            |> List.sort (fun (_, used_in) (_, used_in') ->
-              List.length used_in - List.length used_in')
-            |> List.hd
-          in
-          if List.length irs = List.length used_in
+          let atoms_set = Set.of_list atoms in
+          if atoms_set |> Set.is_empty
           then orig_ir
           else (
-            let atoms = List.filter (fun atom -> atom <> atom_to_move) atoms in
-            let irs_used, irs_free =
-              irs
-              |> List.mapi (fun i ir -> i, ir)
-              |> List.partition (fun (i, ir) -> List.mem i used_in)
+            let irs_using_var : (int * atom Set.t) list =
+              List.mapi
+                begin fun i ir ->
+                  let free_vars = collect_free ir in
+                  let used_vars = Set.inter atoms_set free_vars in
+                  i, used_vars
+                end
+                irs
             in
-            let irs_used = List.map snd irs_used in
-            let irs_free = List.map snd irs_free in
-            let ir = land_ (exists [ atom_to_move ] (land_ irs_used) :: irs_free) in
-            if atoms <> [] then exists atoms ir else ir))
-      | Exists (atoms, Lor irs) -> lor_ (List.map (exists atoms) irs)
-      | ir -> ir)
-    ir
+            let var_is_used_in : (atom, int list) Map.t =
+              List.map
+                begin fun atom ->
+                  ( atom
+                  , List.filter_map
+                      (fun (i, s) ->
+                         if
+                           Set.mem s atom
+                           || (Config.config.antiprenex_mode = `Push_re
+                               && List.nth irs i
+                                  |> function
+                                  | SRegRaw _ -> true
+                                  | _ -> false)
+                         then Some i
+                         else None)
+                      irs_using_var )
+                end
+                atoms
+              |> Map.of_alist_exn
+            in
+            let atom_to_move, used_in =
+              var_is_used_in
+              |> Map.to_alist
+              |> List.sort (fun (_, used_in) (_, used_in') ->
+                List.length used_in - List.length used_in')
+              |> List.hd
+            in
+            if List.length irs = List.length used_in
+            then orig_ir
+            else (
+              let atoms = List.filter (fun atom -> atom <> atom_to_move) atoms in
+              let irs_used, irs_free =
+                irs
+                |> List.mapi (fun i ir -> i, ir)
+                |> List.partition (fun (i, ir) -> List.mem i used_in)
+              in
+              let irs_used = List.map snd irs_used in
+              let irs_free = List.map snd irs_free in
+              let ir = land_ (exists [ atom_to_move ] (land_ irs_used) :: irs_free) in
+              if atoms <> [] then exists atoms ir else ir))
+        | Exists (atoms, Lor irs) -> lor_ (List.map (exists atoms) irs)
+        | ir -> ir)
+      ir
 ;;
 
 (* let simpl1 ir = 
