64e1e642e8ca625022f78af2c42473a21f4a9ffb
Nikita Samodurov <nikita98351@gmail.com>
2026-07-27 13:21:51 +0300

    feat: add flags for gaussqe

--- lib/Config.ml
+++ lib/Config.ml
@@ -26,6 +26,7 @@ type config =
   ; mutable pre_simpl : bool
   ; mutable quiet : bool
   ; mutable simpl_alpha : bool
+  ; mutable simpl_quantifier_elim : bool
   ; mutable simpl_mono : bool
   ; mutable stop_after : [ `Pre_dpll | `Pre_simplify | `Simpl | `Solving ]
   ; mutable under_approx : int
@@ -65,6 +66,7 @@ let config =
   ; no_str_bv = false
   ; quiet = false
   ; simpl_alpha = false
+  ; simpl_quantifier_elim = false
   ; simpl_mono = true
   ; with_check_sat = false
   ; with_info = true
@@ -276,7 +278,13 @@ Basic options:
       , "\tDon't try simplifications based on alpha-equivalence" )
     ; ( "--alpha"
       , Arg.Unit (fun () -> config.simpl_alpha <- true)
-      , "\tDO simplifications based on alpha-equivalence" ) *)
+         , "\tDO simplifications based on alpha-equivalence" ) *)
+    ; ( "--qelim"
+      , Arg.Unit (fun () -> config.simpl_quantifier_elim <- true)
+      , "\tApply quantifier elimination for linear systems" )
+    ; ( "--no-qelim"
+      , Arg.Unit (fun () -> config.simpl_quantifier_elim <- false)
+      , "\tDon't use quantifier elimination" )
     ; ( "--over-nfa"
       , Arg.Unit (fun () -> config.over_nfa <- true)
       , "\tOverapproximate orderings within the NFA Solver" )
